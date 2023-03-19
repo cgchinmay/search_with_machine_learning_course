ac0459654eb16b1231b83b83ee40578349c668a3
cgchinmay <chinmay5j@gmail.com>
2023-03-19 16:20:50 +0000

    Task2 level2

--- utilities/query.py
+++ utilities/query.py
@@ -11,7 +11,7 @@ from urllib.parse import urljoin
 import pandas as pd
 import fileinput
 import logging
-
+import fasttext
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -47,15 +47,26 @@ def create_prior_queries(doc_ids, doc_id_weights,
                 pass  # nothing to do in this case, it just means we can't find priors for this doc
     return click_prior_query
 
+def get_filters(categories):
+    return [
+        {
+            "terms": {
+                "categoryPathIds": categories
+                }
+        }
+        ]
 
 # Hardcoded query here.  Better to use search templates or other query config.
-def create_query(user_query, click_prior_query, filters, sort="_score", sortDir="desc", size=10, source=None, use_synonym=False):
+def create_query(user_query, click_prior_query, filters, sort="_score", sortDir="desc", size=10, source=None, use_synonym=False, add_categories=False):
     field_name = "name"
     if use_synonym:
         field_name = "name.synonyms"
 
+    if not add_categories:
+        filters = []
+        
     query_obj = {
-        'size': 1,
+        'size': size,
         "sort": [
             {sort: {"order": sortDir}}
         ],
@@ -192,11 +203,28 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
     return query_obj
 
 
-def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc", use_synonym=False):
+def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc", use_synonym=False, add_categories=False):
     #### W3: classify the query
+    model = fasttext.load_model('/workspace/datasets/fasttext/model_queries.bin')
+    predictions, scores = model.predict(user_query)
+    categories = []
+    min_score = 0.5
+    curr_score = 0
+    for category, score in zip(predictions, scores):
+        formatted_category = category.replace("__label__","")
+        categories.append(formatted_category)
+
+        curr_score += score
+        if curr_score > min_score:
+            break
+        
+    print(f"categories: {categories}")
+
     #### W3: create filters and boosts
+    filters = get_filters(categories)
+
     # Note: you may also want to modify the `create_query` method above
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"], use_synonym=use_synonym)
+    query_obj = create_query(user_query, click_prior_query=None, filters=filters, sort=sort, sortDir=sortDir, source=["name", "shortDescription"], use_synonym=use_synonym, add_categories=add_categories)
     logging.info(query_obj)
     print(query_obj)
     response = client.search(query_obj, index=index)
@@ -220,6 +248,7 @@ if __name__ == "__main__":
     general.add_argument('--user',
                          help='The OpenSearch admin.  If this is set, the program will prompt for password too. If not set, use default of admin/admin')
     general.add_argument("-syn", '--synonyms', type=bool, default=False, help='Use sysnonym if set')
+    general.add_argument("-cat", '--categories', type=bool, default=False, help='Use categories if set')
     args = parser.parse_args()
 
     if len(vars(args)) == 0:
@@ -254,6 +283,6 @@ if __name__ == "__main__":
         query = line.rstrip()
         if query == "Exit":
             break
-        search(client=opensearch, user_query=query, index=index_name, use_synonym=args.synonyms)
+        search(client=opensearch, user_query=query, index=index_name, use_synonym=args.synonyms, add_categories=args.categories)
 
     
